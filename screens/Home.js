afc1fd12318d1e84e0873fdd51292c0e4262110f
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-10-22 14:12:27 +0100

    Working on expense tracker

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,38 @@
 import React from "react";
-import {Text, View} from "react-native";
+import {Text, View, TouchableOpacity, Image} from "react-native";
+
 
 
 import {COLORS, FONTS, SIZES, icons} from '../constants';
 
 const Home =() => {
+
+    function renderNavBar(){
+        return(
+            <View>
+                <TouchableOpacity>
+                    <Image 
+                    source={icons.back_arrow}
+                    style={{height: 30, width:30, tintColor: COLORS.primary}}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Image 
+                    source={icons.more}
+                    style={{height: 30, width:30, tintColor: COLORS.primary}}
+                    />
+                </TouchableOpacity>
+            </View>
+        )
+
+    }
+
     return(
-        <View>
-             <Text>Home</Text> 
+        <View style={{flex: 1, backgroundColor: COLORS.lightGray}}>
+             {/* <Text>Home</Text>  */}
+             {/* Creating a Nav Bar Section */}
+             {renderNavBar()}
         </View>
     )
 }
