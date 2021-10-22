f1dbe9ed405a384a9b79a47e98d293ccdad5bddc
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-10-22 09:37:33 +0100

    Working on expense tracker

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style={styles.screen}>
+     
+
+       <View
+        style={styles.colorRed}>
+        <Text>1</Text>
+      </View> 
+
+      <View
+        style={styles.colorPink}>
+        <Text>2</Text>
+      </View> 
+
+       <View
+        style={styles.colorGreen}>
+        <Text>3</Text>
+      </View> 
+      {/* <Text>Hello</Text> */}
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    flexDirection: 'row', 
+    width: '80%', 
+    height: 300, 
+    justifyContent: 'space-between', 
+    alignItems: 'stretch'
   },
+
+  colorRed:{
+    backgroundColor: 'red',
+    //  width: 100,
+    //  height: 100,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  colorPink:{
+    backgroundColor: 'pink',
+    // width: 100,
+    // height: 100,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  colorGreen:{
+    backgroundColor: 'green',
+    // width: 100,
+    // height: 100,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 50,
+    alignItems: 'center'
+  },
+
+  input: { width: '80%', borderBottomColor: 'black', borderBottomWidth: 1, padding: 10 }
+
 });
---
+++ constants/icons.js
@@ -0,0 +1,35 @@
+export const up_arrow = require("../assets/icons/up.png");
+export const down_arrow = require("../assets/icons/down.png");
+export const back_arrow = require("../assets/icons/back_arrow.png");
+export const chart = require("../assets/icons/chart_icon.png");
+export const clothing = require("../assets/icons/clothing.png");
+export const savings = require("../assets/icons/savings.png");
+export const misc = require("../assets/icons/misc.png");
+export const transportation = require("../assets/icons/transportation_icon.png");
+export const feeding = require("../assets/icons/feeding_icon.png");
+export const healthcare = require("../assets/icons/healthcare.png");
+export const more = require("../assets/icons/more.png");
+
+
+
+export default{
+    up_arrow,
+    down_arrow,
+    back_arrow,
+    chart,
+    clothing,
+    savings,
+    misc,
+    transportation,
+    feeding,
+    healthcare,
+    more
+
+}
+
+
+
+
+
+
+
