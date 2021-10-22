766253dcc416aef992d28d121847957b21c343c3
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-10-22 13:36:41 +0100

    Working on expense tracker

--- App.js
+++ App.js
@@ -1,74 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack"
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View
-      style={styles.screen}>
-     
+import {Home} from './screens';
 
-       <View
-        style={styles.colorRed}>
-        <Text>1</Text>
-      </View> 
+const theme ={
+  ...DefaultTheme,
+  color:{
+    ...DefaultTheme.colors,
+    border:"transparent"
 
-      <View
-        style={styles.colorPink}>
-        <Text>2</Text>
-      </View> 
-
-       <View
-        style={styles.colorGreen}>
-        <Text>3</Text>
-      </View> 
-      {/* <Text>Hello</Text> */}
-      <StatusBar style="auto" />
-    </View>
-  );
+  }
 }
 
-const styles = StyleSheet.create({
-  screen: {
-    flexDirection: 'row', 
-    width: '80%', 
-    height: 300, 
-    justifyContent: 'space-between', 
-    alignItems: 'stretch'
-  },
+const Stack = createStackNavigator();
 
-  colorRed:{
-    backgroundColor: 'red',
-    //  width: 100,
-    //  height: 100,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  colorPink:{
-    backgroundColor: 'pink',
-    // width: 100,
-    // height: 100,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  colorGreen:{
-    backgroundColor: 'green',
-    // width: 100,
-    // height: 100,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
 
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingBottom: 50,
-    alignItems: 'center'
-  },
+const App = () => {
+  return(
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+          screenOptions={{
+            headerShown: false
+          }}
+          initialRouteName={'Home'}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
 
-  input: { width: '80%', borderBottomColor: 'black', borderBottomWidth: 1, padding: 10 }
+  )
+} 
 
-});
+export default App;
---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+
+import {COLORS, FONTS, SIZES, icons} from '../constants';
+
+const Home =() => {
+    return(
+        <View>
+             <Text>Home</Text> 
+        </View>
+    )
+}
+
+export default Home;
