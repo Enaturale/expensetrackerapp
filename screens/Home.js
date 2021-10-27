17356ea417a8ae65c03d212f261a8d838330b902
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-10-27 10:03:32 +0100

    Home

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,5 @@
 import React, {useState} from "react";
-import {Text, View, TouchableOpacity, Image, ScrollView, FlatList} from "react-native";
-
-
-
-
+import {Text, View, TouchableOpacity, Image, ScrollView, FlatList, StyleSheet} from "react-native";
 
 import {COLORS, FONTS, SIZES, icons} from '../constants';
 
@@ -196,7 +192,9 @@ const Home =() => {
     ]
 
     const[categories, setCategories] = useState(Categories);
-    const [viewMode, setViewMode] = useState("chart")
+    const [viewMode, setViewMode] = useState("chart");
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [showMoreToggle, setShowMoreToggle] = useState(false);
 
     function renderNavBar(){
         return(
@@ -324,12 +322,21 @@ const Home =() => {
     function renderCategoryList(){
         const renderItem=({item}) =>{
             return(
-                <TouchableOpacity style={{flex:1, flexDirection:'row', margin:5,
+                <TouchableOpacity 
+                    style={{
+                     flex:1, 
+                     flexDirection:'row', 
+                     margin:5,
                      paddingVertical: SIZES.radius, 
                      paddingHorizontal: SIZES.padding,
                      borderRadius: 5, 
                      backgroundColor: COLORS.peach,
-                     borderColor: COLORS.primary }}>
+                     borderColor: COLORS.primary,
+                     ...styles.shadow}}
+
+                     onPress={() => setSelectedCategory(item)}
+                     
+                     >
                     <Image 
                         source={item.icon}
                         style={{
@@ -338,7 +345,9 @@ const Home =() => {
                             paddingRight: 2,
                         }}
                     />
-                    <Text style={{marginLeft: SIZES.base, color: COLORS.primary}}>{item.name}</Text>
+                    <Text style={{marginLeft: SIZES.base, color: COLORS.primary, ...FONTS.h4}}>
+                        {item.name}
+                    </Text>
                 </TouchableOpacity>
             )
         }
@@ -353,6 +362,21 @@ const Home =() => {
                     numColumns={2}                 
                     />
                 </View>
+
+                <TouchableOpacity
+                  style={{
+                      flexDirection:'row',
+                      marginVertical: SIZES.base,
+                      justifyContent:'center',
+                  }}
+                  onPress={() => {
+                      setShowMoreToggle(!showMoreToggle)
+                  }}                  
+                  >
+                      <Text style={{...FONTS.body4}}>{showMoreToggle ? "Less" : "More"}</Text>
+                      <Image source={ showMoreToggle ? icons.up_arrow : icons.down_arrow } 
+                        style={{marginLeft:5, width:15, height:15, alignSelf:'center', color: COLORS.red}} />
+                </TouchableOpacity>
             </View>
         )
     }
@@ -387,5 +411,19 @@ const Home =() => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    shadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 2,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 3
+    }
+
+})
 
 export default Home;
